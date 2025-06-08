63783eb3441c2e72688879fe6d5138d0928cb7c6
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-08 08:48:37 +0000

    changes

--- datacollection/settings.py
+++ datacollection/settings.py
@@ -209,7 +209,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.10",
     "http://0.0.0.0",
     "https://shalom-enterprise-1.onrender.com",
-    'iheardatacollection.onrender.com'
+    'iheardatacollection.onrender.com',
   'https://iheardatacollection.onrender.com',
 
     ]
