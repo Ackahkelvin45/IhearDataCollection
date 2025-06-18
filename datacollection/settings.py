4bb94a908fc90defd4370f57394b87b79fda16fc
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-18 17:05:10 +0000

    chanhes

--- datacollection/settings.py
+++ datacollection/settings.py
@@ -168,8 +168,8 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL")  # Make sure this matches exactly
 EMAIL_HOST_PASSWORD =  os.getenv("EMAIL_PASSWORD")
 
