e046cef9e3c8e66afd548f694f27b38a5b502c67
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-30 16:08:15 +0000

    celery

--- data/signals.py
+++ data/signals.py
@@ -2,10 +2,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import NoiseDataset
-from .tasks import process_audio_file
+from .tasks import process_audio_task
 
 @receiver(post_save, sender=NoiseDataset)
 def trigger_audio_processing(sender, instance, created, **kwargs):
     if not instance.audio:
         return
-    process_audio_file.delay(instance.id)
+    process_audio_task.delay(instance.id)
