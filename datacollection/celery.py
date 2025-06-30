016c6668a384016c806efcb2e6a1521b4b546b54
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-30 15:24:00 +0000

    celery

--- datacollection/celery.py
+++ datacollection/celery.py
@@ -1,16 +1,14 @@
 import os
 from celery import Celery
 import logging
-from celery.schedules import crontab
 
 
 logger = logging.getLogger(__name__)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "datacollection.settings")
 
-app = Celery("iheardatacollection")
+app = Celery("datacollection")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.conf.beat_schedule = {}
 
 
 app.autodiscover_tasks()
